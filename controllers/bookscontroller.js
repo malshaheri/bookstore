97c5d57fdc2c0f46108555e14805a2bfad6bafec
malshaheri <malshaheri@gmail.com>
2022-08-09 16:22:40 +0200

    adding books

--- controllers/bookscontroller.js
+++ controllers/bookscontroller.js
@@ -1,29 +1,56 @@
-const books = require ('../data/books.json')[0].books
+const books = require('../data/books.json')[0].books
+const authors = require('../data/authors.json')
+const fs = require('fs')
+const path = require('path')
 
-
-
-const booksHandler = (req, res) => {
-  res.render("mainTemplate", {
-    title: "books",
-    content: "books",
-    books: books
-  })
+const booksHandler = (req, res)=>{
+    res.render('mainTemplate', {
+        title: "Books",
+        content: "books",
+        books: books
+    })
 }
 
 const bookHandler = (req, res)=>{
-  // bookId= req.params.bookId
-  // 1- search/find the book in books array, which matching the bookId
-  console.log('incomming request, processing...')
-  let book = books.find(b=>b.id == req.params.bookId)
-  // 2- if the book exist:
-  if(book){
-      // render the bbok view, with the book object
-      res.render('mainTemplate', {title: book.title, content: 'book', book: book})
-  }else{
-      // render error content
-      res.status(404).render('mainTemplate', {title: "NOT FOUND", content: '404'})
-  }
+    // bookId= req.params.bookId
+    // 1- search/find the book in books array, which matching the bookId
+    console.log('incomming request, processing...')
+    let book = books.find(b=>b.id == req.params.bookId)
+    // 2- if the book exist:
+    if(book){
+        // render the book view, with the book object
+        res.render('mainTemplate', {title: book.title, content: 'book', book: book})
+    }else{
+        // render error content
+        res.status(404).render('mainTemplate', {title: "NOT FOUND", content: '404'})
+    }
 }
 
 
-module.exports = { booksHandler, bookHandler };
+const addBookGetHandler = (req, res)=>{
+    res.render('mainTemplate', {title: "Add Book", content: "addBook", authors: authors})
+}
+const addBookPostHandler = (req, res)=>{
+    console.log(req.body)
+    const book = req.body
+    // add an id to this new book
+    book.id = books.length>0? books[books.length - 1].id +1:0
+    //Search for the author, maching authorId
+    let authorName = authors.find(au=> au.id === book.authorId).name
+    book.author = authorName
+    // ready to store
+    books.push(book)
+    let contentFile = [{books: books}]
+    // now store this content to the file
+    fs.writeFile(path.join(__dirname, "../data/books.json"), JSON.stringify(contentFile), error=>{
+        if(error){
+            res.json(error)
+        }else{
+            res.redirect('/books')
+        }
+    })
+    //res.json(book)
+}
+
+
+module.exports = {booksHandler, bookHandler, addBookGetHandler, addBookPostHandler}
--- routes/books.js
+++ routes/books.js
@@ -1,5 +1,5 @@
 const express= require('express')
-const {booksHandler, bookHandler} = require('../controllers/bookscontroller')
+const {booksHandler, bookHandler, addBookGetHandler, addBookPostHandler} = require('../controllers/bookscontroller')
 const router = express.Router()
 
 // create a controller "bookscontroller.js" to handle the get request for books hendler= booksHandler
@@ -11,6 +11,8 @@ router.get('/', booksHandler)
 // localhost:3000/books/book/4
 router.get('/book/:bookId', bookHandler)
 
-
+// add book router
+router.get('/add', addBookGetHandler)
+router.post('/add', addBookPostHandler)
 
 module.exports = router
